cd63fb7919808393e510f0d4c08ba64cb8e917cf
fc51773 <fc51773@alunos.fc.ul.pt>
2022-10-14 18:57:08 +0100

    some documentation

--- mastermind-android/lytics/src/main/java/com/thelion/lytics/builders/ConcreteEventBuilder.java
+++ mastermind-android/lytics/src/main/java/com/thelion/lytics/builders/ConcreteEventBuilder.java
@@ -3,6 +3,9 @@ package com.thelion.lytics.builders;
 import com.thelion.lytics.events.ConcreteEvent;
 import com.thelion.lytics.helpers.Parameter;
 
+/**
+ * An event builder for concrete events in which any parameters plus a name can be put
+ */
 public class ConcreteEventBuilder {
     private ConcreteEvent event;
 
@@ -10,15 +13,31 @@ public class ConcreteEventBuilder {
         event = new ConcreteEvent();
     }
 
+    /**
+     *  a builder of events
+     * @return the builder of events
+     */
     public static ConcreteEventBuilder builder() {
         return new ConcreteEventBuilder();
     }
 
+    /**
+     * sets a name for event
+     * @param name name of the event (like init, session, match)
+     * @return the builder of event
+     */
     public ConcreteEventBuilder withName(String name) {
         event.setName(name);
         return this;
     }
 
+    /**
+     * Adds a new parameter to the concrete event
+     * @param key a key (string) for the parameter key
+     * @param value a value if any type for the content of the parameter
+     * @param <T> Any type for the value of the parameter
+     * @return the builder with a event added with new parameter
+     */
     public <T> ConcreteEventBuilder withParameter(String key, T value) {
         event.addParameter(new Parameter<>(key, value));
         return this;
